83a1c6b28eada22a5093482521f8aeed26da378f
Jacob Stannix <jakestannix at gmail dot com>
2021-05-26 07:42:42 -0600

    added function to update ioctl

--- src/mbr.rs
+++ src/mbr.rs
@@ -1,5 +1,6 @@
 //! This module houses all of the fucitons related to the formating of Master Boot Record partitions
 // use std::path::Path;
+use gptman::linux;
 use mbrman;
 /// This fuction is designed to be used in conjunciton with an already formated disk.
 /// # Panics
@@ -25,65 +26,72 @@ pub fn list_partitions(disk: String) {
 /// * make_swap: if true creates a swap partition with size of swap_size
 pub fn basic_arch_part(user_disk: String, make_swap: bool, swap_size: usize) {
     let mut f = std::fs::File::create(&user_disk).expect("could not open disk");
-    let mut mbr  = mbrman::MBR::new_from(&mut f, 512, [0x01, 0x02, 0x03, 0x04])
-	.expect("could not make partition table");
-    mbr.write_into(&mut f)
-	.expect("could not write mbr to disk");
+    let mut mbr = mbrman::MBR::new_from(&mut f, 512, [0x01, 0x02, 0x03, 0x04])
+        .expect("could not make partition table");
+    mbr.write_into(&mut f).expect("could not write mbr to disk");
 
     let mut f = std::fs::File::open(&user_disk).expect("could not open disk");
-    let mut mbr = mbrman::MBR::read_from(&mut f, 512)
-	.expect("could not find MBR");
-    let free_partition_number = mbr.iter().find(|(_i, p)| p.is_unused()).map(|(i, _)| i)
-	.expect("no more places avalible");
-    let sectors = mbr.get_maximum_partition_size()
-	.expect("no more space avalible");
-    let starting_lba = mbr.find_optimal_place(sectors)
-	.expect("could not find a place to put the partition");
+    let mut mbr = mbrman::MBR::read_from(&mut f, 512).expect("could not find MBR");
+    let free_partition_number = mbr
+        .iter()
+        .find(|(_i, p)| p.is_unused())
+        .map(|(i, _)| i)
+        .expect("no more places avalible");
+    let sectors = mbr
+        .get_maximum_partition_size()
+        .expect("no more space avalible");
+    let starting_lba = mbr
+        .find_optimal_place(sectors)
+        .expect("could not find a place to put the partition");
 
     if !make_swap {
-    mbr[free_partition_number] = mbrman::MBRPartitionEntry {
-	boot: true,
-	first_chs: mbrman::CHS::empty(),
-	sys: 0x83,
-	last_chs: mbrman::CHS::empty(),
-	starting_lba,
-	sectors,
-    };
-	// create drive file so we can write to it
-    let mut f = std::fs::File::create(&user_disk).expect("could not read disk");
-    mbr.write_into(&mut f)
-	.expect("could not write MBR to disk");
-    }else {
-    mbr[free_partition_number] = mbrman::MBRPartitionEntry {
-	boot: false,
-	first_chs: mbrman::CHS::empty(),
-	sys: 0x82,
-	last_chs: mbrman::CHS::empty(),
-	starting_lba,
-	sectors: swap_size as u32,
-    };
+        mbr[free_partition_number] = mbrman::MBRPartitionEntry {
+            boot: true,
+            first_chs: mbrman::CHS::empty(),
+            sys: 0x83,
+            last_chs: mbrman::CHS::empty(),
+            starting_lba,
+            sectors,
+        };
+        // create drive file so we can write to it
+        let mut f = std::fs::File::create(&user_disk).expect("could not read disk");
+        mbr.write_into(&mut f).expect("could not write MBR to disk");
+    let mut f = std::fs::File::open(&user_disk).expect("could not open disk");
+        linux::reread_partition_table(&mut f).unwrap();
+    } else {
+        mbr[free_partition_number] = mbrman::MBRPartitionEntry {
+            boot: false,
+            first_chs: mbrman::CHS::empty(),
+            sys: 0x82,
+            last_chs: mbrman::CHS::empty(),
+            starting_lba,
+            sectors: swap_size as u32,
+        };
 
-    let free_partition_number = mbr.iter().find(|(_i, p)| p.is_unused()).map(|(i, _)| i)
-	.expect("no more places avalible");
-    let sectors = mbr.get_maximum_partition_size()
-	.expect("no more space avalible");
-    let starting_lba = mbr.find_optimal_place(sectors)
-	.expect("could not find a place to put the partition");
+        let free_partition_number = mbr
+            .iter()
+            .find(|(_i, p)| p.is_unused())
+            .map(|(i, _)| i)
+            .expect("no more places avalible");
+        let sectors = mbr
+            .get_maximum_partition_size()
+            .expect("no more space avalible");
+        let starting_lba = mbr
+            .find_optimal_place(sectors)
+            .expect("could not find a place to put the partition");
 
-    mbr[free_partition_number] = mbrman::MBRPartitionEntry {
-	boot: true,
-	first_chs: mbrman::CHS::empty(),
-	sys: 0x83,
-	last_chs: mbrman::CHS::empty(),
-	starting_lba,
-	sectors,
-    };
+        mbr[free_partition_number] = mbrman::MBRPartitionEntry {
+            boot: true,
+            first_chs: mbrman::CHS::empty(),
+            sys: 0x83,
+            last_chs: mbrman::CHS::empty(),
+            starting_lba,
+            sectors,
+        };
 
-    let mut f = std::fs::File::create(&user_disk).expect("could not read disk");
-    mbr.write_into(&mut f)
-	.expect("could not write MBR to disk");
+        let mut f = std::fs::File::create(&user_disk).expect("could not read disk");
+        mbr.write_into(&mut f).expect("could not write MBR to disk");
+    let mut f = std::fs::File::open(&user_disk).expect("could not open disk");
+        linux::reread_partition_table(&mut f).unwrap();
     }
-    
 }
-
-
