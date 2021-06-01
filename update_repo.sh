7cf05a3c57f0aa70bd542b75ac7f241328dde187
delta1024 <68131520+delta1024@users.noreply.github.com>
2021-05-31 19:11:12 -0600

    Delete update_repo.sh

--- update_repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env zsh
-arch_repo=$HOME/NAS/archRepo/x86_64
-makepkg -f
-rm $arch_repo/rali-testing*.pkg.tar.zst
-mv rali-*.pkg.tar.zst $arch_repo/
-pushd $arch_repo/..
-./update-repo.sh
-popd
